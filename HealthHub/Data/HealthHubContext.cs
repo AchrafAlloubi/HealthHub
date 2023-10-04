e0f6bbb6c44f99afe6774987aedfeeb9edf1195b
AchrafAlloubi <achrafalloubi@gmail.com>
2023-10-04 12:44:23 -0400

    Create Models and Data Context

---
+++ HealthHub/Data/HealthHubContext.cs
@@ -0,0 +1,25 @@
+using HospitalMS.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalMS.Data
+{
+    public class HealthHubContext : DbContext
+    {
+        public HealthHubContext(DbContextOptions<HealthHubContext> options)
+: base(options)
+        {
+        }
+        public virtual DbSet<Patient> Patients { get; set; }
+        public virtual DbSet<Appointment> Appointments { get; set; }
+        public virtual DbSet<Room> Rooms { get; set; }
+        public virtual DbSet<Admission> Admissions { get; set; }
+        public virtual DbSet<Discharge> Discharges { get; set; }
+        public virtual DbSet<Physician> Physicians { get; set; }
+        public virtual DbSet<Specialty> Specialties { get; set; }
+        public virtual DbSet<Country> Countries { get; set; }
+    }
+}
